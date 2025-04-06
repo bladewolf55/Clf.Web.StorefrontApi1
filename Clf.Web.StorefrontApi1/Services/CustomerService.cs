1e562ab9dd47841b04490f43a6b7d84755422275
Charles Flatt <charles@tfhome.us>
2025-04-06 07:45:11 -0600

    Remove explicit CustomerService mapping

--- Clf.Web.StorefrontApi1/Services/CustomerService.cs
+++ Clf.Web.StorefrontApi1/Services/CustomerService.cs
@@ -1,6 +1,7 @@
 ﻿using Clf.Web.StorefrontApi1.Domain.RepositoryInterfaces;
 using Clf.Web.StorefrontApi1.Domain.DomainModels;
 using Clf.Web.StorefrontApi1.Domain.Services;
+using Clf.Web.StorefrontApi1.Domain.DataMapping;
 
 namespace Clf.Web.StorefrontApi1.Services;
 
@@ -9,8 +10,7 @@ public class CustomerService : ICustomerService
     private readonly ILogger<CustomerService> logger;
     private readonly ICustomerRepository customerRepository;
 
-    public CustomerService(ILogger<CustomerService> logger, 
-        ICustomerRepository customerRepository)
+    public CustomerService(ILogger<CustomerService> logger, ICustomerRepository customerRepository)
     {
         this.logger = logger;
         this.customerRepository = customerRepository;
@@ -23,37 +23,7 @@ public class CustomerService : ICustomerService
         var ordersDto = customerDto.Orders;
 
         // mapping
-        var customer = new Customer()
-        {
-            Id = customerDto.Id,
-            Name = customerDto.Name
-        };
-        if (billingAddressDto != null)
-        {
-            customer.BillingAddress = new Address()
-            {
-                AddressType = billingAddressDto.AddressType,
-                Address1 = billingAddressDto.Address1,
-                Address2 = billingAddressDto.Address2,
-                City = billingAddressDto.City,
-                State = billingAddressDto.State,
-                Zip = billingAddressDto.Zip
-            };
-        }
-        customer.Orders = ordersDto.Select(dto => new Order()
-        {
-            Number = dto.Number,
-            OrderedOn = dto.OrderedOn,
-            ShipTo = dto.ShipTo == null ? null : new Address()
-            {
-                AddressType = dto.ShipTo.AddressType,
-                Address1 = dto.ShipTo.Address1,
-                Address2 = dto.ShipTo.Address2,
-                City = dto.ShipTo.City,
-                State = dto.ShipTo.State,
-                Zip = dto.ShipTo.Zip
-            },
-        }).ToList();
+        var customer = customerDto.ToDomain();
 
         if (!customer.IsValid())
         {
